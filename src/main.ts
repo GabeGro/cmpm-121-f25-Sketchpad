1a169a14e1f38417f0e50e586b22871c165ba296
GabeGro <ggroenwo@ucsc.edu>
2025-10-17 02:59:12 +0000

    step 1 complete: added rounded canvas

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,3 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+const _canvas = document.getElementById("canvas");
