6f81396b9440c91e0ed4fd135e0eca54db43ca1a
GabeGro <ggroenwo@ucsc.edu>
2025-10-18 00:09:57 +0000

    step 3 completed: program now clears and redraws every time a change occurs using arrays and event dispatcher

--- src/main.ts
+++ src/main.ts
@@ -10,32 +10,73 @@ canvas.height = 500;
 document.body.append(canvas);
 
 const ctx = canvas.getContext("2d")!;
-ctx.lineWidth = 2;
+ctx.lineWidth = 3;
 ctx.strokeStyle = "black";
+
+type Point = { x: number; y: number };
+let lines: Point[][] = [];
+let currentLine: Point[] | null = null;
 
 const cursor = { active: false, x: 0, y: 0 };
 
-//store cursor pos1
 canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
   cursor.x = e.offsetX;
   cursor.y = e.offsetY;
+
+  //assign lines to reference currentLine and add first point
+  currentLine = [];
+  lines.push(currentLine);
+  if (currentLine) {
+    currentLine.push({
+      x: cursor.x,
+      y: cursor.y,
+    });
+  }
+
+  //call event to draw first stroke
+  //canvas.dispatchEvent(new CustomEvent("drawing-changed"));
 });
 
-//when cursor moves to pos2, draw line between pos1 and pos2
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
-    ctx.beginPath();
-    ctx.moveTo(cursor.x, cursor.y);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+    //add new coords to to currentLine while the cursor is down
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    currentLine?.push({
+      x: cursor.x,
+      y: cursor.y,
+    });
+
+    canvas.dispatchEvent(new CustomEvent("drawing-changed"));
   }
 });
 
 canvas.addEventListener("mouseup", () => {
+  //stop adding coords and reset currentLine
   cursor.active = false;
+  currentLine = null;
+
+  //canvas.dispatchEvent(new CustomEvent("drawing-changed"));
+});
+
+//clear canvas and redraw based on coords from lines
+canvas.addEventListener("drawing-changed", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (const line of lines) {
+    if (line.length > 1) {
+      ctx.beginPath();
+      const { x, y } = line[0];
+      ctx.moveTo(x, y);
+
+      for (const { x, y } of line) {
+        ctx.lineTo(x, y);
+      }
+
+      ctx.stroke();
+    }
+  }
 });
 
 const clearButton = document.createElement("button");
@@ -43,5 +84,6 @@ clearButton.innerHTML = "clear";
 document.body.append(clearButton);
 
 clearButton.addEventListener("click", () => {
-  ctx.clearRect(0, 0, canvas?.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  lines = [];
 });
